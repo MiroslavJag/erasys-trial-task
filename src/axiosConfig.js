92af36f95c00a0c0ca5671f49ab12e429010a902
Miroslav Jagodic <miroslav.jagodic@oroundo.com>
2019-04-29 23:58:18 +0200

    Created infinite scroll on Profiles page

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-export const GET_PROFILES = 'search?length=10'
+export const GET_PROFILES = 'search?length=12'
 export const GET_PROFILE = 'profiles?ids='
 export const GET_ALL_PROFILES_IDS = 'profiles?'
 export const GET_IDS = '&ids='
+export const GET_CURSOR = '&cursor='
 
 const apiInstance = axios.create({
     baseURL: 'http://localhost:3000/api/',
--- src/pages/Profiles/index.js
+++ src/pages/Profiles/index.js
@@ -1,39 +1,70 @@
 import React, { Component } from 'react';
-import axios, {GET_PROFILES, GET_ALL_PROFILES_IDS, GET_IDS} from '../../axiosConfig';
+import axios, {GET_PROFILES, GET_ALL_PROFILES_IDS, GET_IDS, GET_CURSOR} from '../../axiosConfig';
 import ProfileCard from '../../components/ProfileCard'
+
 
 class Profiles extends Component {
     state = {
         items: null,
+        updatedItems: null,
+        cursors: null,
         loading: true,
-        error: false
+        error: false,
+        total: null
     }
 
-    createProfilesIdQuery = (items) => {
+    handleScroll = () => {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const windowBottom = windowHeight + window.pageYOffset + 1;
+        if (windowBottom >= docHeight) {
+            if (this.state.updatedItems.length >= this.state.total){
+                return null
+            }
+            this.getProfiles(this.state.cursors)
+        } else {
+        }
+    }
+
+    createProfilesUpdateQuery = (items) => {
         let queryItemsIds = items.map(item => GET_IDS + item.id)
         return GET_ALL_PROFILES_IDS + queryItemsIds.toString().slice(1).replace(/,/g , "");
     }
 
-    componentDidMount () {
-        axios.get(GET_PROFILES)
+    getProfiles = (cursors) => {
+        axios.get(cursors ? (GET_PROFILES + GET_CURSOR + cursors) : GET_PROFILES)
             .then( response => {
-                this.setState( { loading: false, items: response.data.items })
-                axios.get(this.createProfilesIdQuery(response.data.items))
+                this.setState( {
+                    items: response.data.items,
+                    cursors: response.data.cursors.after,
+                    total: response.data.total
+                })
+                axios.get(this.createProfilesUpdateQuery(response.data.items))
                     .then(response => {
-                        let updatedItems = [...this.state.items]
-                        updatedItems.map((item, index) => {
+                        let itemsUpdate = [...this.state.items]
+                        itemsUpdate.map((item, index) => {
                             item["distance"] = response.data[index].location.distance
                             item["headline"] = response.data[index].headline
                         })
-                        this.setState({items: updatedItems})
+                        this.setState({
+                            updatedItems: this.state.updatedItems
+                                ? this.state.updatedItems.concat(itemsUpdate) : itemsUpdate,
+                            loading: false
+                        })
                     })
             }).catch( error => {
-                this.setState({error: true, loading: false});
-                console.log('ERROR: ',error)
-            } );
-
-
+            this.setState({error: true, loading: false});
+            console.log('ERROR: ',error)
+        } );
     }
+
+    componentDidMount () {
+        this.getProfiles()
+        window.addEventListener("scroll", this.handleScroll)
+    }
+
     render () {
        let profilePreview = null;
         if (this.state.error) {
@@ -41,16 +72,12 @@ class Profiles extends Component {
         }
         else if (this.state.loading) {
             profilePreview = <h2>Content is Loading...</h2>
-        } else {
-            profilePreview = this.state.items.map(item => {
+        }else {
+            profilePreview = this.state.updatedItems.map(item => {
                 return <ProfileCard {...item} key={item.id} param={item.id}/>
             })
         }
-        return (
-            <>
-                {profilePreview}
-            </>
-        )
+        return <>{profilePreview}</>
     }
 }
 
